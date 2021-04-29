41baa1978c0feee1b2a4864c5a26d903b71a6941
hoangvan1410 <hlevan1410@gmail.com>
2021-04-29 15:31:45 +0700

    upload v2

--- src/entities/HealthInfo.js
+++ src/entities/HealthInfo.js
@@ -1,6 +1,6 @@
 module.exports = class HealthInfo {
-    constructor (height, weight, age, gender, medic_his){
-        this.id = null;
+    constructor (id=null,height, weight, age, gender, medic_his){
+        this.id = id;
         this.height = height;
         this.weight = weight;
         this.age = age;
---
+++ src/frameworks/persistence/InMemory/MongoDBHealthInfoRepository.js
@@ -0,0 +1,29 @@
+const HealthInfoRepository = require('../../../application/contracts/HealthInfoRepository')
+
+module.exports = class InMemoryHealthInfoRepository extends HealthInfoRepository{
+
+    constructor(){
+        super();
+    }
+
+    async add(healthInfoInstance){
+       const {height, weight, age, gender, medic_his} = healthInfoEntity;
+       const mongooseHealthInfo = new 
+        return healthInfoInstance;
+    }
+
+    async getById(healthInfoId){
+        let healthInfo;
+        try{
+            const id = parseInt(healthInfoId);
+            healthInfo = this.healthInfos.find(x => x.id ===id);
+        }catch(err){
+            throw new Error('Error Occurred');
+        }
+        return healthInfo;
+    }
+    
+    async getAll(){
+        return this.healthInfos;
+    }
+};
