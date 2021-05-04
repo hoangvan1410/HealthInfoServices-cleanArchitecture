7d455b2c32350b0fe2b01636e825cd8ebb73d303
hoangvan1410 <76679911+hoangvan1410@users.noreply.github.com>
2021-05-04 14:54:49 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require('make-promises-safe')
 const container = require('./infrastructure/config/container')()
 const server = require('./infrastructure/webserver/fastify')(container.cradle)
 
-server.listen(process.env.PORT || 3033, (err, address) => {
+server.listen((process.env.PORT || 3033), (err, address) => {
   if (err) {
     server.log.error(err)
     process.exit(1)
