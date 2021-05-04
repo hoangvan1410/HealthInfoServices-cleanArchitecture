faae581846474470dde093077ef8e6667e95a934
hoangvan1410 <76679911+hoangvan1410@users.noreply.github.com>
2021-05-04 15:31:04 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const container = require('./infrastructure/config/container')()
 const server = require('./infrastructure/webserver/fastify')(container.cradle)
 const PORT = process.env.PORT || 3033
 
-server.listen(ProcessingInstruction, (err, address) => {
+server.listen(PORT, (err, address) => {
   if (err) {
     server.log.error(err)
     process.exit(1)
